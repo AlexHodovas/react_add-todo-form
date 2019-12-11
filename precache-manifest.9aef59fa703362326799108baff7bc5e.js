44a2d8af5f5ad577b8173269adc89cc9f5cc9d03
AlexHodovas <o.hodovas@gmail.com>
2019-12-11 19:55:36 +0200

    Updates

---
+++ precache-manifest.9aef59fa703362326799108baff7bc5e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "778478404b0144783501a30ef7512460",
+    "url": "/react_add-todo-form/index.html"
+  },
+  {
+    "revision": "00de46292c8fc70157b1",
+    "url": "/react_add-todo-form/static/css/main.f85cc46d.chunk.css"
+  },
+  {
+    "revision": "8c62e9e39566e04c3c1e",
+    "url": "/react_add-todo-form/static/js/2.263e448f.chunk.js"
+  },
+  {
+    "revision": "00de46292c8fc70157b1",
+    "url": "/react_add-todo-form/static/js/main.17835cc2.chunk.js"
+  },
+  {
+    "revision": "4953b3d381bec0cda918",
+    "url": "/react_add-todo-form/static/js/runtime~main.9d188fa2.js"
+  }
+]);
